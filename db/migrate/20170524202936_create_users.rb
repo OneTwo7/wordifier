42eead9fea3e44db892b71d24df451060f7ccacf
onetwo7 <lahoohal@gmail.com>
2017-05-25 14:43:59 +0300

    Finish user CRUD actions

--- db/migrate/20170524202936_create_users.rb
+++ db/migrate/20170524202936_create_users.rb
@@ -1,10 +1,11 @@
 class CreateUsers < ActiveRecord::Migration[5.0]
   def change
     create_table :users do |t|
-      t.string :name
-      t.string :email, index: true, unique: true
-      t.string :password_digest
-      t.string :remember_digest
+      t.string  :name
+      t.string  :email, index: true, unique: true
+      t.string  :password_digest
+      t.string  :remember_digest
+      t.boolean :admin, default: false
 
       t.timestamps
     end
